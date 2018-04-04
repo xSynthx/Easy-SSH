5127ebe78fe122db76d256d250e2c69601b23a73
Nytelife26 <xtylerjrx@gmail.com>
2018-04-04 19:17:34 +0100

    Update easy_ssh.py
    Fixed a few bugs on while statements and added in encoding to fix the copyright symbols (and in general, check diff for more)

--- easy_ssh.py
+++ easy_ssh.py
@@ -1,64 +1,68 @@
+# -*- coding: utf-8 -*-
+
 ##################################################################################
-#  ©2017 Synthx                                                                  #
+#  2017 Synthx																	#
 #  You may not redistribute as yours unless you have written consent from Synthx #
-#  Twitter: @_synthx                                                             #
-#  Email: synthx@protonmail.com or synthx01@gmail.com                            #
+#  Twitter: @_synthx															 #
+#  Email: synthx@protonmail.com or synthx01@gmail.com							 #
 ##################################################################################
 
-import readline # arrow keys
 import os # This imports the OS module.
+if os.name != "nt": # readline is not a thing on Windows
+	import readline # arrow keys
 import subprocess # No more spawning a new shell.
 
 alias = {"cancel": ["cancel", "back", "return"], "help": ["help", "?", "/help"], "quit": ["exit", "stop", "quit"]} # Command aliases
 
 def clear():
 	if os.name == "nt":
-		subprocess.call("cls.exe") # CLS in Windows actually clears, not print newlines
+		subprocess.call("cls", shell=True) # CLS in Windows actually clears, not print newlines
 	else:
 		print("\n" * 80)
 
 exit = False
 color = {
-    'RED'             : '\033[1;91m',
-    'UNDERLINE_PURPLE' : '\033[4;34m',
-    'GREEN'           : '\033[1;92m',
-    'YELLOW'          : '\033[1;33m',
-    'CYAN'            : '\033[0;36m',
-    'PURPLE'            : '\033[0;34m',
-    'MAGENTA'         : '\033[0;35m',
-    'DEFAULT'         : '\033[0m',
-    'TWITTER_BLUE'            : '\033[38;5;33m',
+	'RED'			  : '\033[1;91m',
+	'UNDERLINE_PURPLE' : '\033[4;34m',
+	'GREEN'			  : '\033[1;92m',
+	'YELLOW'		  : '\033[1;33m',
+	'CYAN'			  : '\033[0;36m',
+	'PURPLE'			: '\033[0;34m',
+	'MAGENTA'		  : '\033[0;35m',
+	'DEFAULT'		  : '\033[0m',
+	'TWITTER_BLUE'			  : '\033[38;5;33m',
 }
 exitSentence = color['RED'] + '[Now Exiting!] ' + color['DEFAULT'] + 'Easy SSH\n'
 credits = '©2017 Synthx\n' + color['TWITTER_BLUE'] + 'Twitter: @_Synthx' + color['DEFAULT'] + '\n@danbatiste\n@Nytelife26'
 printHelp = color['PURPLE'] + '''
 +-------+----------------------------+
-| help  | Prints this dialog         |
+| help	| Prints this dialog		 |
 +-------+----------------------------+
-| SSH   | Will begin ssh selection   |
+| SSH	| Will begin ssh selection	 |
 +-------+----------------------------+
-| clear | Clears the screen          |
+| clear | Clears the screen			 |
 +-------+----------------------------+
-| exit  | Exits Easy SSH :/          |
+| exit	| Exits Easy SSH :/			 |
 +-------+----------------------------+
-| info  | Shows credits for Easy SSH |
+| info	| Shows credits for Easy SSH |
 +-------+----------------------------+
 '''
 header = color['PURPLE'] + '''
-                  _____                  _____ _____ _   _ 
-                 |  ___|                /  ___/  ___| | | |
-                 | |__  __ _ ___ _   _  \ `--.\ `--.| |_| |
-                 |  __|/ _` / __| | | |  `--. \`--. \  _  |
-                 | |__| (_| \__ \ |_| | /\__/ /\__/ / | | |
-                 \____/\__,_|___/\__, | \____/\____/\_| |_/
-                                  __/ |                    
-                                 |___/                     
-''' + color['DEFAULT'] + '''                        +-----------------------+\n                        |  Created by ''' + color['TWITTER_BLUE'] + '''@_Synthx''' + color['DEFAULT'] + '''  |\n                        +-----------------------+
+				  _____					 _____ _____ _	 _ 
+				 |	___|				/  ___/	 ___| | | |
+				 | |__	__ _ ___ _	 _	\ `--.\ `--.| |_| |
+				 |	__|/ _` / __| | | |	 `--. \`--. \  _  |
+				 | |__| (_| \__ \ |_| | /\__/ /\__/ / | | |
+				 \____/\__,_|___/\__, | \____/\____/\_| |_/
+								  __/ |					   
+								 |___/					   
+''' + color['DEFAULT'] + '''						+-----------------------+\n						   |  Created by ''' + color['TWITTER_BLUE'] + '''@_Synthx''' + color['DEFAULT'] + '''	|\n						   +-----------------------+
 '''
 clear()
 print(header + 'Type "help" to begin!')
 def Main():
 	global exit
+	mainInput = ""
 	while not mainInput: # checking a string as bool returns False if the string is "", so mainInput == "" can just be replaced with boolean mainInput
 		mainInput = input(color['RED'] + 'Easy SSH> ' + color['DEFAULT']).lower() # .lower'd automatically for efficiency
 	if mainInput in alias["help"]:
@@ -86,10 +90,10 @@ def Main():
 		Main()
 def sshStart():
 	global exit
-	mainInputSSH = input(color['RED'] + 'IP> ' + color['DEFAULT'])
+	mainInputSSH = ""
 	while not mainInputSSH:
 		mainInputSSH = input(color['RED'] + 'IP> ' + color['DEFAULT']).lower()
-	elif mainInputSSH in alias["cancel"]:
+	if mainInputSSH in alias["cancel"]:
 		clear()
 		print(header + 'Type "help" to begin!')
 		Main()
@@ -99,10 +103,10 @@ def sshStart():
 		sshUser(mainInputSSH)
 def sshUser(SSH_IP):
 	global exit
-	mainInputUser = input(color['RED'] + 'User> ' + color['DEFAULT']
+	mainInputUser = ""
 	while not mainInputUser:
 		mainInputUser = input(color['RED'] + 'User> ' + color['DEFAULT']).lower()
-	elif mainInputUser in alias["cancel"]:
+	if mainInputUser in alias["cancel"]:
 		clear()
 		print(header + 'Type "help" to begin!')
 		Main()
@@ -113,9 +117,4 @@ def sshUser(SSH_IP):
 		Main()
 if __name__ == "__main__": # Check if we're being run normally or imported
 	while not exit:
-    		Main()
-
-
-
-
-
+		Main()
